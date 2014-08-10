542fa87ef124807c4c8686b52e4a932035791d42
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2014-08-10 23:46:13 +0530

    adds branches to adming

--- colleges/admin.py
+++ colleges/admin.py
@@ -1,6 +1,7 @@
 """ modeladmins """
 
 from django.contrib import admin
-from .models import College
+from .models import College, Branch
 
 admin.site.register(College)
+admin.site.register(Branch)
